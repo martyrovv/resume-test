fa2612a28da7f2d2b4a151f5e522364c3a909020
Marat Martin <marat.mvrtin@email.ru>
2025-07-17 22:12:15 +0300

    resolve: merge conflicts

--- src/components/ResumeForm.tsx
+++ src/components/ResumeForm.tsx
@@ -65,6 +65,7 @@ export function ResumeForm(props: Props) {
   const handleDeleteSection = (id: string) => {
     setSections(sections.filter((section) => section.id !== id));
   };
+  //очистку данных формы для конкретной секции 
 
   const filteredOptions = OPTIONS.filter(
     (option) => !sections.some((sections) => sections.type === option.value)
